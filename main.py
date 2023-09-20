a2e728b758fbfde3ed5ff1f98f91273817a68796
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2023-09-20 12:28:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ for index in range(0,3,1):
     basic.show_number(index) # count up by 1
     basic.pause((1000)
 
-for angle in range(10,0,-2):
+angle = 10
+while angle >0:
     basic.show_number(angle) # count down by -2
+    basic.pause(1000)
+    angle -= 2
