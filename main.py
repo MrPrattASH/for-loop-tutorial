8e160e576f50717729a9725fb6d37dafd5f8618c
Brogan Pratt <101632496+MrPrattASH@users.noreply.github.com>
2023-09-20 12:23:40 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,6 @@
+for index in range(0,3,1):
+    basic.show_number(index) # count up by 1
+    basic.pause((1000)
+
+for angle in range(10,0,-2):
+    basic.show_number(angle) # count down by -2
---
+++ main.ts
@@ -0,0 +1,3 @@
+for (let index = 0; index < 3; index += 1) {
+    basic.showNumber(index)
+}
